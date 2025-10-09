3a9654eae1471e41749dd2c4410dd2404014e2d2
Haider Ali <197620915+mybabyapp122@users.noreply.github.com>
2025-10-09 22:34:30 +0500

    Update footer.blade.php

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,6 +3,6 @@
 ***********************************-->
 <div class="footer footer-outer">
     <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">DexignLab</a> 2023</p>
+        <p>Copyright © MYBABY  {{date('Y')}}</p>
     </div>
 </div>
